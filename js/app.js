5257b2ef5e3d22393d2de570fad5338c42900059
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2015-05-15 15:18:36 -0300

    listo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 // App
+
+$('document').ready(function() {
+	$(".royalSlider").royalSlider({
+		controlNavigation: 'bullets',
+		arrowsNav: true,
+		arrowsNavAutoHide: false,
+		arrowsNavHideOnTouch: false,
+		autoPlay: {
+			enabled: true,
+			pauseOnHover: false,
+			delay: 5000
+		},
+		keyboardNavEnabled: true,
+		loop: true
+	});
+
+	$(".product-details-description").organicTabs({
+		"speed": 200
+	});
+
+	$('.pregunta').each(function(){
+		var cp = new Spry.Widget.CollapsiblePanel($(this)[0], {
+			contentIsOpen: false
+		});
+	});
+
+
+
+});
